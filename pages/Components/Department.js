220cc0ccf70bf0849f52c87b92f6cbe8862572b5
Venkataraman-Nagarajan <venkataraman@motorq.com>
2021-11-19 19:05:24 +0530

    Added Depts section

---
+++ pages/Components/Department.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../../styles/Department.module.css";
+import { Col } from 'react-bootstrap';
+
+const Department = ({ department = {
+    name: "",
+    discord: "",
+    description: "",
+    id: "",
+    icon: "",
+    alt: ""
+  }, }) => {
+    return (
+        <Col md="4" lg="4">
+            <div className={styles.main}>
+                <div className={styles.service}>
+                    <div className={styles.service_logo}>
+                        {/* <Image src={department.icon} alt={department.alt}  /> */}
+                        <img src={department.icon} alt={department.alt} />
+                    </div>
+                    <h4>{department.name}</h4>
+                    <p>{department.description}</p>
+                </div>
+            </div>
+        </Col>
+    );
+}
+
+export default Department;
--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,10 @@ import Head from 'next/head';
 import styles from '../styles/about.module.css';
 import Navbar from './Components/Navbar';
 import Domain from './Components/Domain';
+import Department from './Components/Department';
 import domains from '../data/domains.json';
+import departments from '../data/departments.json';
+import { Container, Row } from 'react-bootstrap';
 
 export default function About() {
 
@@ -17,7 +20,19 @@ export default function About() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
       </Head>
       <Navbar />
+
       <main>
+        <div>
+          <Container className="mt-10" className={styles.departmentContainer}>
+            <Row>
+              {
+                departments.map((department, index) => {
+                  return <Department key={index} department={department} />
+                })
+              }
+            </Row>
+          </Container>
+        </div>
         <div>
           {
             domains.map((domain, i) => {
