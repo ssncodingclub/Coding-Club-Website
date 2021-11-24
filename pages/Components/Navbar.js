ccc002f093cfea3d70b2208ccc9b6cbe1977c48e
adityakp15 <aditya@Adityas-MacBook-Pro.local>
2021-11-24 18:48:06 +0530

    mobile navbar design

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -73,6 +73,9 @@ function NavBar() {
               <ul className={styles.mobilenavul + " " + styles.menu} >
                 <li><a href="/">Home</a></li>
                 <li ><a href="/about">About</a></li>
+                <li ><a href="/about">Meets</a></li>
+                <li ><a href="/about">Projects</a></li>
+                <li ><a href="/about">Blog</a></li>
               </ul>
             </div>
           </Nav>
