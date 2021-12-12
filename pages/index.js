ffd3681d9c0b753ae03354cab12e7a7d97ec5c55
vanathi-g <gvanathi21@gmail.com>
2021-12-12 20:47:30 +0530

    Resolve issues after merging main into this branch

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Head from "next/head";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { BsChevronDoubleDown } from "react-icons/bs";
 import cardDetails from "../data/cardInfo.json";
-import styles from "../styles/Home.module.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Tile from "./Components/Tile";
-import cardDetails from "../data/cardInfo.json";
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
     <div className={styles.parallax}>
@@ -31,8 +31,48 @@ export default function Home() {
             </a>
           </div>
         </main>
-        <div>{/* events,upskill,community section */}</div>
-        <div>{/* thriving community */}</div>
+        <div id="info">
+          <Container className={styles.infoContainer}>
+            <Row>
+              {cardDetails.map((cardInfo, i) => {
+                return (
+                  <Col md="6" sm="12" lg="4">
+                    <Card className={styles.infoContainerCard}>
+                      <Card.Img variant="top" src={cardInfo.img} />
+                      <Card.Body>
+                        <Card.Title className={styles.infoContainerCardTitle}>
+                          {cardInfo.title}
+                        </Card.Title>
+                        <Card.Text> {cardInfo.description}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          </Container>
+        </div>
+        <div>
+          <div className={styles.thrivingCommunityHeaderContainer}>
+            <img src="./HomePage/TC_1.svg" alt="Title-part-1" />
+            <img src="./HomePage/TC_2.svg" alt="Title-part-2" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut
+              tellus quis lacus laoreet commodo. Vivamus velit dolor, tempor id
+              venenatis id, viverra quis velit.{" "}
+            </p>
+            <Button className={styles.joinUsButton}>
+              <a href="#">Join Us</a>
+            </Button>
+          </div>
+          <Container>
+            <img
+              src="./HomePage/discord.png"
+              alt="Discord screenshot"
+              className={styles.discordImage}
+            />
+          </Container>
+        </div>
         <div className={styles.domainSection}>
           {/* lots of domains */}
           <Row>
@@ -97,7 +137,7 @@ export default function Home() {
             </Col>
           </Row>
           <Row className={styles.domainRow}>
-            <Col sm="12" md="5" className={styles.domainImgContainer}>
+            <Col s m="12" md="5" className={styles.domainImgContainer}>
               <img
                 src="./HomePage/ml.png"
                 className={styles.domainImages}
@@ -118,7 +158,29 @@ export default function Home() {
             </Col>
           </Row>
         </div>
-        <div>{/* open source */}</div>
+        <div className={styles.opensourceSection}>
+          <div className={styles.gradientDiv}>
+            <div className={styles.opensourceDiv}>
+              <h1>Oops! We do</h1>
+              <h1>Open Source too</h1>
+              <h6>
+                Lorem ipsum dolor sit amet, <br />
+                consectetur epsit.
+              </h6>
+              <button className={styles.openSourceButton}>Check it out</button>
+              <img
+                src="./HomePage/guy1.png"
+                alt="Title"
+                className={styles.openSourceImg1}
+              />
+              <img
+                src="./HomePage/guy2.png"
+                alt="Title"
+                className={styles.openSourceImg2}
+              />
+            </div>
+          </div>
+        </div>
         <div className={styles.tileSection}>
           <Container fluid>
             <Row className={styles.tileRow}>
@@ -150,123 +212,7 @@ export default function Home() {
             </Row>
           </Container>
         </div>
-        <Container>
-          <img
-            src="./HomePage/discord.png"
-            alt="Discord screenshot"
-            className={styles.discordImage}
-          />
-        </Container>
       </div>
-      <div className={styles.domainSection}>
-        {/* lots of domains */}
-        <Row>
-          <Col sm="12" md="6">
-            <img
-              src="./HomePage/LoD.png"
-              className={styles.domainHeader}
-              alt="Title"
-            />
-          </Col>
-        </Row>
-        <Row className={styles.domainRow}>
-          <Col sm="12" md="5" className={styles.domainImgContainer}>
-            <img
-              src="./HomePage/cp.png"
-              className={styles.domainImages}
-              alt="Title"
-            />
-            <h4 className={styles.domainName}>Competitive Programming</h4>
-          </Col>
-          <Col sm="12" md="7" className={styles.domainInfoContainer}>
-            <p className={styles.domainInfo}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut
-              tellus quis lacus laoreet commodo. Vivamus velit dolor, tempor id
-              venenatis id, viverra quis velit. <br />
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut
-              tellus quis lacus laoreet commodo. Vivamus velit dolor, tempor id
-              venenatis id, viverra quis velit.{" "}
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col
-            xs={{ order: "last" }}
-            sm={{ order: "last" }}
-            md={{ span: 7, order: "first" }}
-            className={styles.domainInfoContainer}
-          >
-            <p className={styles.domainInfo}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut
-              tellus quis lacus laoreet commodo. Vivamus velit dolor, tempor id
-              venenatis id, viverra quis velit. <br />
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut
-              tellus quis lacus laoreet commodo. Vivamus velit dolor, tempor id
-              venenatis id, viverra quis velit.{" "}
-            </p>
-          </Col>
-          <Col
-            xs={{ order: "first" }}
-            sm={{ order: "first" }}
-            md={{ span: 5, order: "last" }}
-            className={styles.domainImgContainer}
-          >
-            <img
-              src="./HomePage/sd.png"
-              className={styles.domainImages}
-              alt="Title"
-            />
-            <h4 className={styles.domainName}>Software Development</h4>
-          </Col>
-        </Row>
-        <Row className={styles.domainRow}>
-          <Col s m="12" md="5" className={styles.domainImgContainer}>
-            <img
-              src="./HomePage/ml.png"
-              className={styles.domainImages}
-              alt="Title"
-            />
-            <h4 className={styles.domainName}>Machine Learning</h4>
-          </Col>
-          <Col sm="12" md="7" className={styles.domainInfoContainer}>
-            <p className={styles.domainInfo}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut
-              tellus quis lacus laoreet commodo. Vivamus velit dolor, tempor id
-              venenatis id, viverra quis velit. <br />
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut
-              tellus quis lacus laoreet commodo. Vivamus velit dolor, tempor id
-              venenatis id, viverra quis velit.{" "}
-            </p>
-          </Col>
-        </Row>
-      </div>
-      <div className={styles.opensourceSection}>
-        <div className={styles.gradientDiv}>
-          <div className={styles.opensourceDiv}>
-            <h1>Oops! We do</h1>
-            <h1>Open Source too</h1>
-            <h6>
-              Lorem ipsum dolor sit amet, <br />
-              consectetur epsit.
-            </h6>
-            <button className={styles.openSourceButton}>Check it out</button>
-            <img
-              src="./HomePage/guy1.png"
-              alt="Title"
-              className={styles.openSourceImg1}
-            />
-            <img
-              src="./HomePage/guy2.png"
-              alt="Title"
-              className={styles.openSourceImg2}
-            />
-          </div>
-        </div>
-      </div>
-      <div>{/* non stop events+footer */}</div>
       <div className={styles.placeholder}></div>
       <Footer />
     </div>
