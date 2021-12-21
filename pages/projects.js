2595ff2cb1ac2562289b3f03408de5b7f73477f5
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-12-21 16:49:54 +0530

    removed right pane

--- pages/projects.js
+++ pages/projects.js
@@ -4,29 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
 import Navbar from "./Components/Navbar";
 
-const ExpandedCard = () => {
-  return (
-    <div className={styles.ExpandedCard}>
-      <div className="imageContainer">
-        <img src="https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y29kZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
-      </div>
-      <div className={styles.subheader_}>
-        <h1>Project Title</h1>
-      </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Hendrerit dolor magna eget est lorem ipsum dolor sit amet.
-        Aliquam faucibus purus in massa tempor nec feugiat nisl. Malesuada bibendum arcu vitae
-        elementum curabitur vitae nunc. Magnis dis parturient montes nascetur ridiculus mus mauris
-        vitae. Blandit massa enim nec dui. Interdum posuere lorem ipsum dolor sit amet consectetur
-        adipiscing. Sed risus ultricies tristique nulla aliquet enim. Venenatis tellus in metus
-        vulputate eu scelerisque. Egestas pretium aenean pharetra magna. Commodo quis imperdiet
-        massa tincidunt nunc pulvinar sapien. Eros in cursus turpis
-      </p>
-      <a href="/"> Check it out </a>
-    </div>
-  );
-};
+const ExpandedCard = () => {};
 
 const ProjectCard = ({ name, techStack, domain, projectImage, id }) => {
   const classNamePrefix = "projectcard_item_",
@@ -132,9 +110,7 @@ const Projects = () => {
             ))}
           </div>
 
-          <div className={styles.right_pane}>
-            <ExpandedCard />
-          </div>
+          <div className={styles.right_pane}></div>
         </div>
 
         {/* <Footer /> */}
