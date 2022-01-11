5c30bc39149c97019d1f3ec3d68deecd975973a8
shashankp4nda <shashankpanda24@gmail.com>
2022-01-11 20:08:27 +0530

    Added on hover gradient effect to footer icons

--- pages/Components/Footer.js
+++ pages/Components/Footer.js
@@ -6,29 +6,30 @@ import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
 function Footer() {
     return (
         <footer className={styles.footerContainer}>
-
             <div className={styles.footer}>
             <h1>Join us today!</h1>
             <h2>Try! Catch! Show!</h2>
             <div className={styles.social_media}>
                 <a 
-                  href="https://www.linkedin.com/company/ssn-coding-club/?originalSubdomain=in"
+                  href="https://www.linkedin.com/company/ssn-coding-club/?originalSubdomain=in" 
                   target="_blank" 
                   rel="noreferrer" 
-                ><FaLinkedin className={styles.social_icon} /></a>
+                  className={`fa fa-linkedin ${styles.social_icon}`}
+                ></a> 
                 <a 
                   href="https://github.com/ssncodingclub"
                   target="_blank" 
                   rel="noreferrer" 
-                ><FaGithub className={styles.social_icon} /></a>
+                  className={`fa fa-github ${styles.social_icon}`}
+                ></a>
                 <a 
                   href="https://www.instagram.com/ssn_coding_club/"
                   target="_blank" 
                   rel="noreferrer"
-                ><FaInstagram className={styles.social_icon} /></a>
-            </div>  
+                  className={`fa fa-instagram ${styles.social_icon}`}
+                ></a>
+          </div>
             </div>
-          
         </footer>
     );
 }
