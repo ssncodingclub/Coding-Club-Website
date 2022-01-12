b75d21604e803c9e501918d44c85c07eb35a6a63
shashankp4nda <shashankpanda24@gmail.com>
2022-01-12 17:04:14 +0530

    Changed footer heading styling

--- pages/Components/Footer.js
+++ pages/Components/Footer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "../../styles/Footer.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
 
 function Footer() {
     return (
         <footer className={styles.footerContainer}>
             <div className={styles.footer}>
-            <h1>Join us today!</h1>
-            <h2>Try! Catch! Show!</h2>
+              <div className={styles.footerText}>
+                <h1>Join us today!</h1>
+                <h2>Try! Catch! Show!</h2>
+              </div>
             <div className={styles.social_media}>
                 <a 
                   href="https://www.linkedin.com/company/ssn-coding-club/?originalSubdomain=in" 
