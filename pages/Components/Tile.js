1ee2025a8bf0e4602cd907552ab3f21670cf3cf9
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2022-01-12 23:00:34 +0530

    added link

--- pages/Components/Tile.js
+++ pages/Components/Tile.js
@@ -3,7 +3,7 @@ import styles from "../../styles/Tile.module.css";
 function Tile({ tileColor, tileImage }) {
     return (
         <div className={styles.tileContent + " " + styles[tileColor]} >
-            <img src={".././tiles/" + tileImage} className={styles.img}></img>
+            <a href="https://www.instagram.com/ssn_coding_club/"><img src={".././tiles/" + tileImage} className={styles.img}></img></a>
             {/* <h1>Title</h1>
             <h2>Subtitle</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut tellus quis lacus laoreet commodo.</p> */}
