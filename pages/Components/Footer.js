6f30d26eca382dbac0ce29510bf5ff7613501926
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2022-01-12 18:13:10 +0530

    added discord icon

--- pages/Components/Footer.js
+++ pages/Components/Footer.js
@@ -1,37 +1,35 @@
 import React from "react";
+import { FaDiscord, FaGithub, FaLinkedin } from "react-icons/fa";
 import styles from "../../styles/Footer.module.css";
-
 function Footer() {
-    return (
-        <footer className={styles.footerContainer}>
-            <div className={styles.footer}>
-              <div className={styles.footerText}>
-                <h1>Join us today!</h1>
-                <h2>Try! Catch! Show!</h2>
-              </div>
-            <div className={styles.social_media}>
-                <a 
-                  href="https://www.linkedin.com/company/ssn-coding-club/?originalSubdomain=in" 
-                  target="_blank" 
-                  rel="noreferrer" 
-                  className={`fa fa-linkedin ${styles.social_icon}`}
-                ></a> 
-                <a 
-                  href="https://github.com/ssncodingclub"
-                  target="_blank" 
-                  rel="noreferrer" 
-                  className={`fa fa-github ${styles.social_icon}`}
-                ></a>
-                <a 
-                  href="https://www.instagram.com/ssn_coding_club/"
-                  target="_blank" 
-                  rel="noreferrer"
-                  className={`fa fa-instagram ${styles.social_icon}`}
-                ></a>
-          </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.footer}>
+        <div className={styles.footerText}>
+          <h1>Join us today!</h1>
+          <h2>Try! Catch! Show!</h2>
+        </div>
+        <div className={styles.social_media}>
+          <a href="https://in.linkedin.com/company/ssn-coding-club" className={styles.linkedin}>
+            <FaLinkedin className={styles.social_icon} />
+          </a>
+          <a href="https://github.com/ssncodingclub" className={styles.github}>
+            <FaGithub className={styles.social_icon} />
+          </a>
+          <a
+            href="https://www.instagram.com/ssn_coding_club/"
+            target="_blank"
+            rel="noreferrer"
+            className={`fa fa-instagram ${styles.social_icon}`}
+          ></a>
+          <a href="https://discord.com/invite/hSGEaRUnws" className={styles.discord}>
+            <FaDiscord className={styles.social_icon} />
+          </a>
+
+        </div>
+      </div>
+    </footer >
+  );
 }
 
 export default Footer;
