825d43cac297e572714c1ee454069a85f62c63d0
Akash <akashrangesh07@gmail.com>
2022-01-25 11:37:10 +0530

    added light mode for home page

--- pages/about.js
+++ pages/about.js
@@ -29,7 +29,7 @@ export default function About() {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
       </Head>
-      <Navbar />
+      <Navbar theme={()=> {}} hide={false} />
 
       <main className={styles.about_main_container}>
         {isAppleDevice ? (
