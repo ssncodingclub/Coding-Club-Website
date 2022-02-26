99a606cf507a6a6159ac7c7b82c5a4e246fb42ba
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2022-02-26 15:57:19 +0530

    server side code for blog files rendering

---
+++ pages/blog/[slug].js
@@ -0,0 +1,49 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { Remarkable } from "remarkable";
+
+export default function Post({ frontmatter: { title, date, cover_image }, slug, content }) {
+  const md = new Remarkable();
+  return (
+    <>
+      <div className="card card-page">
+        <h1 className="post-title">{title}</h1>
+        <div className="post-body">
+          <div dangerouslySetInnerHTML={{ __html: md.render(content) }}></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("_posts"));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+  console.log(paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  console.log(slug);
+  const markdownWithMeta = fs.readFileSync(path.join("_posts", slug + ".md"), "utf-8");
+
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      frontmatter,
+      slug,
+      content,
+    },
+  };
+}
