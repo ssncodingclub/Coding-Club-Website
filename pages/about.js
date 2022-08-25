cb5658ac22e8a444bad442fd0e052cf15e622005
Themozhi <beyondinfinity121@gmail.com>
2022-08-25 19:22:40 +0530

    image wrap

--- pages/about.js
+++ pages/about.js
@@ -58,27 +58,24 @@ export default function About(props) {
                 About SSN
               </h2>
 
-
-              <p className={props.theme ? styles.para_light_main : styles.para_main}>
-                <p><Image className={styles.ssnimg} src="\ssnfountain.jpg" width={200} height={200} />
-                SSN Institutions, established by Mr. Shiv Nadar, Founder-Chairman, HCL Technologies, stands out as a
-                premier center of higher learning with a mission of pursuing excellence in education and research. With
-                an aim to support and foster the thriving coding culture on campus, SSN strives to provide a platform
-                for students to hone their technical skills by encouraging them to pursue cutting-edge technology and
-                software. It serves as a forum for creative output and facilitates exciting opportunities to innovate,
-                collaborate and nurture a zeal for programming . A committed team of faculty and staff play an integral
-                role in the realization of the institution’s quest for educational distinction in computing and emerging
-                fields.</p>
-
-
-                <div className={styles.main}>
-                  <div>Dr. T.T. Mirnalinee - Honorary Member </div>
-                  <div> Dr. Chitra Babu - Club Incharge</div>
-                  <div>Dr. V. Balasubramanian - Club Incharge  </div>
-                </div>
-              
-
-              </p>
+              <div className={styles.box}>
+                <Image className={styles.ssnimg} src="\ssnfountain.jpg" width={200} height={200} />
+                <p className={props.theme ? styles.para_light_main : styles.para_main}>
+                  SSN Institutions, established by Mr. Shiv Nadar, Founder-Chairman, HCL Technologies, stands out as a
+                  premier center of higher learning with a mission of pursuing excellence in education and research. With
+                  an aim to support and foster the thriving coding culture on campus, SSN strives to provide a platform
+                  for students to hone their technical skills by encouraging them to pursue cutting-edge technology and
+                  software. It serves as a forum for creative output and facilitates exciting opportunities to innovate,
+                  collaborate and nurture a zeal for programming . A committed team of faculty and staff play an integral
+                  role in the realization of the institution’s quest for educational distinction in computing and emerging
+                  fields.
+                  <div className={styles.main}>
+                    <div>Dr. T.T. Mirnalinee - Honorary Member </div>
+                    <div> Dr. Chitra Babu - Club Incharge</div>
+                    <div>Dr. V. Balasubramanian - Club Incharge  </div>
+                  </div>
+                </p>
+              </div>
               <h2 className={props.theme ? styles.subheader_light : styles.subheader}>
                 What is SSN Coding Club?
               </h2>
