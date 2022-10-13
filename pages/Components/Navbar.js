142e8f2620d033520980cb72a87aa10d6b7aed00
Themozhi <beyondinfinity121@gmail.com>
2022-10-13 17:36:57 +0530

    removed about ssn page and added faculty details in about page

--- pages/Components/Navbar.js
+++ pages/Components/Navbar.js
@@ -64,15 +64,6 @@ function NavBar(props) {
               <Nav.Item className={styles.nav_item}>About</Nav.Item>
             </Link>
             <Link
-              href="/aboutssn"
-              onClick={() => {
-                setExpanded(false);
-              }}
-              passHref
-            >
-              <Nav.Item className={styles.nav_item}>About SSN</Nav.Item>
-            </Link>
-            <Link
               href="/meets"
               onClick={() => {
                 setExpanded(false);
@@ -137,17 +128,6 @@ function NavBar(props) {
                     passHref
                   >
                     <Nav.Item style={{ cursor: "pointer" }}>About</Nav.Item>
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/aboutssn"
-                    onClick={() => {
-                      setExpanded(false);
-                    }}
-                    passHref
-                  >
-                    <Nav.Item style={{ cursor: "pointer" }}>About SSN</Nav.Item>
                   </Link>
                 </li>
                 <li>
